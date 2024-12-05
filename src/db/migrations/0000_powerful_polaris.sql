f230057c6dffbd574334915219f6512d9d0ec7c7
Ben Ralton <b.ralton@icloud.com>
2024-12-05 23:20:30 +0000

    Pushing SQL changes

--- src/db/migrations/0000_powerful_polaris.sql
+++ src/db/migrations/0000_powerful_polaris.sql
@@ -1,6 +1,3 @@
--- Current sql file was generated after introspecting the database
--- If you want to run this migration please uncomment this code before executing migrations
-/*
 DO $$ BEGIN
  CREATE TYPE "key_status" AS ENUM('expired', 'invalid', 'valid', 'default');
 EXCEPTION
@@ -14,13 +11,7 @@ EXCEPTION
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- CREATE TYPE "aal_level" AS ENUM('aal3', 'aal2', 'aal1');
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- CREATE TYPE "factor_type" AS ENUM('webauthn', 'totp');
+ CREATE TYPE "factor_type" AS ENUM('phone', 'webauthn', 'totp');
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
@@ -32,16 +23,31 @@ EXCEPTION
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
+ CREATE TYPE "aal_level" AS ENUM('aal3', 'aal2', 'aal1');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
  CREATE TYPE "code_challenge_method" AS ENUM('plain', 's256');
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "urls" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"url" text,
-	"alias" varchar(10),
-	"count" integer DEFAULT 0
-);
-
-*/
+DO $$ BEGIN
+ CREATE TYPE "one_time_token_type" AS ENUM('phone_change_token', 'email_change_token_current', 'email_change_token_new', 'recovery_token', 'reauthentication_token', 'confirmation_token');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "equality_op" AS ENUM('in', 'gte', 'gt', 'lte', 'lt', 'neq', 'eq');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "action" AS ENUM('ERROR', 'TRUNCATE', 'DELETE', 'UPDATE', 'INSERT');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
